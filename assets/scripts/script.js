3489b2d20a0c1ba7ba194a34bbfd89142208f3c8
raddymaster <antellitocci@gmail.com>
2021-05-23 16:17:07 -0400

    added javascript to auto populate next 5 days in forecast and minor styling changes)

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -33,7 +33,7 @@ function getCityWeather(lat, long, name){
                 console.log(data);
                 //clear old information for all weather elements (TO DO)
                 //get current info from data
-                var currentDay = moment(data.current.dt, "s").format("L");
+                var currentDay = moment(data.current.dt, "X").format("L");
                 var cityInfo = (name + " (" + currentDay + ") ").toString();
                 console.log(cityInfo);
                 var currentIcon = "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png";
@@ -42,14 +42,20 @@ function getCityWeather(lat, long, name){
                 var currentHumidity = data.current.humidity;
                 var currentUV = data.current.uvi;
 
-                //get future weather information day 1
-                //get future weather information day 2
-                //get future weather information day 3
-                //get future weather information day 4
-                //get future weather information day 5
+                //get future weather information day 1 loop through the array? yes
+                for (var i = 1; i < 6; i ++){
+
+                    var futureDate = moment(data.daily[i].dt, "X").format("L");
+                    var futureIcon = "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + "@2x.png";
+                    var futureTemp = data.daily[i].temp.max;
+                    var futureWind = data.daily[i].wind_speed;
+                    var futureHumid = data.daily[i].humidity;
+                    var futureUV = data.daily[i].uvi;
+
+                    createFiveDayWeather(futureDate, futureIcon, futureTemp, futureWind, futureHumid, futureUV);
+                }
                 //pass current weather information to the current weather info box
                 createCurrentWeatherInfo(cityInfo, currentIcon, currentTemp, currentWind, currentHumidity, currentUV);
-
             });
         }
         else{
@@ -61,53 +67,85 @@ function getCityWeather(lat, long, name){
 
 //create current weather info box (take information from the api)
 function createCurrentWeatherInfo(info, icon, temp, wind, humidity, uvi){
-                //create elements for main weather box
-                var cityInfoElem = $("<h2>").text(info);
-                var cityWeatherIconElem = $("<img>").attr("src", icon);
-                var cityTempElem = $("<h5>").html("Temperature: " + temp + "&#176;F");
-                var cityWindElem = $("<h5>").html("Wind: " + wind + " MPH");
-                var cityHumidElem = $("<h5>").html("Humidity: " + humidity + "%");
-                var cityUVIElem = $("<h5>").text("UV Index: ");
-                var cityUVIInfoElem = $("<span>").text(uvi);
-                //apply class to UVI span element
-                if (uvi <= 2){
-                    cityUVIInfoElem.addClass("is-low");
-                    cityUVIInfoElem.removeClass("is-moderate");
-                    cityUVIInfoElem.removeClass("is-high");
-                    cityUVIInfoElem.removeClass("is-very-high");
-                }
-                else if (uvi > 2 && uvi <= 5){
-                    cityUVIInfoElem.removeClass("is-low");
-                    cityUVIInfoElem.addClass("is-moderate");
-                    cityUVIInfoElem.removeClass("is-high");
-                    cityUVIInfoElem.removeClass("is-very-high");
-                }
-                else if (uvi > 5 && uvi <= 7){
-                    cityUVIInfoElem.removeClass("is-low");
-                    cityUVIInfoElem.removeClass("is-moderate");
-                    cityUVIInfoElem.addClass("is-high");
-                    cityUVIInfoElem.removeClass("is-very-high");
-                }
-                else{
-                    cityUVIInfoElem.removeClass("is-low");
-                    cityUVIInfoElem.removeClass("is-moderate");
-                    cityUVIInfoElem.removeClass("is-high");
-                    cityUVIInfoElem.addClass("is-very-high");                   
-                }
-                //append complimentary items prior to adding everything to info
-                cityInfoElem.append(cityWeatherIconElem);
-                cityUVIElem.append(cityUVIInfoElem);
+    //create elements for main weather box
+    var cityInfoElem = $("<h2>").text(info);
+    var cityWeatherIconElem = $("<img>").attr("src", icon);
+    var cityTempElem = $("<h5>").html("Temperature: " + temp + "&#176;F");
+    var cityWindElem = $("<h5>").html("Wind: " + wind + " MPH");
+    var cityHumidElem = $("<h5>").html("Humidity: " + humidity + "%");
+    var cityUVIElem = $("<h5>").text("UV Index: ");
+    var cityUVIInfoElem = $("<span>").text(uvi);
+    //apply class to UVI span element
+    uviColor(uvi, cityUVIInfoElem);
+    //append complimentary items prior to adding everything to info
+    cityInfoElem.append(cityWeatherIconElem);
+    cityUVIElem.append(cityUVIInfoElem);
 
-                //append items to main weather info section
-                $("#current-weather").append(cityInfoElem);
-                $("#current-weather").append(cityTempElem);
-                $("#current-weather").append(cityWindElem);
-                $("#current-weather").append(cityHumidElem);
-                $("#current-weather").append(cityUVIElem);
+    //append items to main weather info section
+    $("#current-weather").append(cityInfoElem);
+    $("#current-weather").append(cityTempElem);
+    $("#current-weather").append(cityWindElem);
+    $("#current-weather").append(cityHumidElem);
+    $("#current-weather").append(cityUVIElem);
 };
 
-function createFiveDayWeather(){
+function createFiveDayWeather(date, icon, temp, wind, humidity, uvi){
+    //create elements for daily weather boxes
+    var weatherCardCellElem = $("<div>").addClass("cell");
+    var weatherCardElem = $("<div>").addClass("card flex-child-auto").attr("style", "margin: 10px 0px 10px 30px; padding: 10px;");
+    var weatherCardDateElem = $("<h4>").text(date);
+    var weatherCardIconElem = $("<img>").attr("src", icon);
+    weatherCardIconElem.css("height", "60px");
+    weatherCardIconElem.css("width", "60px");
+    var weatherCardTempElem = $("<p>").html("Temperature: " + temp + "&#176;F");
+    var weatherCardWindElem = $("<p>").html("Wind: " + wind + " MPH");
+    var weatherCardHumidElem = $("<p>").html("Humidity: " + humidity + "%");
+    var weatherCardUVIElem = $("<p>").text("UV Index: ");
+    var weatherCardUVISpanElem = $("<span>").text(uvi);
 
+    uviColor(uvi, weatherCardUVISpanElem);
+
+    weatherCardUVIElem.append(weatherCardUVISpanElem);
+
+    weatherCardElem.append(weatherCardDateElem);
+    weatherCardElem.append(weatherCardIconElem);
+    weatherCardElem.append(weatherCardTempElem);
+    weatherCardElem.append(weatherCardWindElem);
+    weatherCardElem.append(weatherCardHumidElem);
+    weatherCardElem.append(weatherCardUVIElem);
+    
+    weatherCardCellElem.append(weatherCardElem);
+
+    $("#daily-weather").append(weatherCardCellElem);
+
+};
+
+function uviColor(uvi, uviElement){
+    //apply class to UVI span element
+    if (uvi <= 2){
+        uviElement.addClass("is-low");
+        uviElement.removeClass("is-moderate");
+        uviElement.removeClass("is-high");
+        uviElement.removeClass("is-very-high");
+    }
+    else if (uvi > 2 && uvi <= 5){
+        uviElement.removeClass("is-low");
+        uviElement.addClass("is-moderate");
+        uviElement.removeClass("is-high");
+        uviElement.removeClass("is-very-high");
+    }
+    else if (uvi > 5 && uvi <= 7){
+        uviElement.removeClass("is-low");
+        uviElement.removeClass("is-moderate");
+        uviElement.addClass("is-high");
+        uviElement.removeClass("is-very-high");
+    }
+    else{
+        uviElement.removeClass("is-low");
+        uviElement.removeClass("is-moderate");
+        uviElement.removeClass("is-high");
+        uviElement.addClass("is-very-high");                   
+    }
 };
 
 getCityCoordinates(cityElem);
